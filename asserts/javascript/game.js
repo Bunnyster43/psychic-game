40f5590352c6bdaf567fc2ef4137f78203f78b55
isaac <simstarr18@yahoo.com>
2018-05-15 18:07:32 -0700

    initial site files

--- asserts/javascript/game.js
+++ asserts/javascript/game.js
@@ -5,29 +5,29 @@ var guessesSoFar = [];
 var computerChoices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9'];
 
 document.onkeyup = function(event) {
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase(); 
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     var computerGuess = computerChoices[Math.floor(Math.random()*computerChoices.length)];
-    guessesSoFar.push(userGuess); 
+    guessesSoFar.push(userGuess);
 
     if (userGuess == computerGuess) {
         wins++;
-        alert('Way to go! You\'ve guesesed corrrectly. You Won!');
-        guessesLeft = 9;
+        alert('You Won!');
+        guessesLeft = 10;
         guessesSoFar.length = 0;
     }
 
     else if (guessesLeft == 0){
         losses++;
-        alert('You didn\'t guess the correct letter. You lost. Let\'s try again.');
-        guessesLeft = 9;
+        alert('You lost.');
+        guessesLeft = 10;
         guessesSoFar.length = 0;
     }
 
     else if (userGuess !== computerGuess){
-        guessesLeft--; 
-    }  
+        guessesLeft--;
+    }
 
-    var html = "<h1>The Psychic Game</h1>" + 
+    var html = "<h1>The Psychic Game</h1>" +
     "<p>Guess what letter I'm thinking of</p>" +
     "<p>Total Wins: " + wins + "</p>" +
     "<p>Total Losses: " + losses + "</p>" +
